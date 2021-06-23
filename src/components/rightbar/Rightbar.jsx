a7f35562697f01a8b175eaf127f94edb9cece270
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-23 02:18:25 +0100

    Making rightbar component conditional for the Profile page and Home page

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,10 +3,11 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-export default function Rightbar() {
-    return (
-        <div className="rightbar">
-            <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+
+    const HomeRightbar = () => {
+        return (
+            <>
                 <div className="birthdayContainer">
                     <img src="/assets/gift.png" alt="" className="birthdayImg" />
                     <span className="birthdayText">
@@ -20,6 +21,64 @@ export default function Rightbar() {
                         <Online user={u} />
                     ))}
                 </ul>
+            </>
+        );
+    };
+
+    const ProfileRightbar = () => (
+        <>
+            <h4 className="rightbarTitle">User Information</h4>
+            <div className="rightbarInfo">
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">City:</span>
+                    <span className="rightbarInfoValue">New York</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">From:</span>
+                    <span className="rightbarInfoValue">Madrid</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">Relationship:</span>
+                    <span className="rightbarInfoValue">Single</span>
+                </div>
+            </div>
+            <h4 className="rightbarTitle">User Fiends</h4>
+            <div className="rightbarFollowings">
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className="rightbarFolloingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className="rightbarFolloingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className="rightbarFolloingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className="rightbarFolloingName">John Carter</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img src="/assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+                    <span className="rightbarFolloingName">John Carter</span>
+                </div>
+            </div>
+        </>
+    );
+
+    const RightbarContent = () => {
+        if (profile)
+            return <ProfileRightbar />;
+        else
+            return <HomeRightbar />;
+    }
+
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                <RightbarContent />
             </div>
         </div>
     )
