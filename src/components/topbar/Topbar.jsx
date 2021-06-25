c00d4a792a1a0669d90fb6ad305d980c1438d74a
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-25 19:39:53 +0100

    Using react router Link component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons"
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
 
@@ -9,7 +10,9 @@ export default function Topbar() {
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
-                <span className="logo">Social</span>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">Social</span>
+                </Link>
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
