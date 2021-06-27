d5e5a9aeeeccd58f7a09a7a839f62eb3b8c4db90
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-27 20:35:10 +0100

    Loading post feeds from api for the selected user profile

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,18 +4,19 @@ import Share from '../share/Share';
 import "./feed.css";
 import axios from "axios";
 
-export default function Feed() {
+export default function Feed({ username }) {
     const [posts, setPosts] = useState([]);
-
     useEffect(() => {
         const fetchPosts = async () => {
-            const res = await axios.get("posts/timeline/60cab0d5bfa12a1280be3a66");
+            const res = username ?
+                await axios.get(`/posts/profile/${username}`) :
+                await axios.get("posts/timeline/60cab0d5bfa12a1280be3a66");
             setPosts(res.data);
         };
 
         fetchPosts();
 
-    }, []);
+    }, [username]);
 
     return (
         <div className="feed">
