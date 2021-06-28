ed8e855d8a92f9d4a32ff2193c665a617e996d47
Behzad G. A <behzad.ghaffarnejad@gmail.com>
2021-06-28 19:20:01 +0100

    Improving Login page & page redirections after login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@material-ui/core";
 
 export default function Login() {
+    const email = useRef();
+    const password = useRef();
+    const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+    const handleLoginClick = (e) => {
+        e.preventDefault();
+        loginCall({
+            email: email.current.value,
+            password: password.current.value
+        },
+            dispatch);
+    };
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -12,13 +28,32 @@ export default function Login() {
                     </span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" type="password" className="loginInput" />
-                        <button className="loginButton">Log In</button>
+                    <form className="loginBox" onSubmit={handleLoginClick}>
+                        <input
+                            placeholder="Email"
+                            type="email"
+                            className="loginInput"
+                            ref={email}
+                            required
+                        />
+                        <input
+                            placeholder="Password"
+                            type="password"
+                            className="loginInput"
+                            ref={password}
+                            required
+                            minLength="6"
+                        />
+                        <button className="loginButton" disabled={isFetching}>
+                            {isFetching ? (
+                                <CircularProgress size="32px" color="white" />
+                            ) :
+                                "Log In"
+                            }
+                        </button>
                         <span className="loginForgot">Forgot Password?</span>
-                        <button className="loginRegisterButton">Create a New Account</button>
-                    </div>
+                        <button className="loginRegisterButton" disabled={isFetching}>Create a New Account</button>
+                    </form>
                 </div>
             </div>
         </div>
