fb249dc653901941ae02cbccdd7ac5fbdc35572d
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-07-17 18:14:38 +0100

    Implementing Register functionality

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
 import "./register.css";
+import { useHistory } from "react-router";
 
 export default function Register() {
+    const username = useRef();
+    const email = useRef();
+    const password = useRef();
+    const passwordAgain = useRef();
+    const history = useHistory();
+
+    const hancleClick = async (e) => {
+        e.preventDefault();
+        const user = {
+            username: username.current.value,
+            email: email.current.value,
+            password: password.current.value
+        };
+
+        try {
+            await axios.post('auth/register', user);
+            history.push('/login');
+        } catch (err) {
+            console.log(err);
+        }
+
+    };
+
+    const checkIfPasswordMatch = (e) => {
+        let message = '';
+        if (password.current.value !== passwordAgain.current.value) {
+            message = "Passwords don't match!";
+        }
+        password.current.setCustomValidity(message);
+    };
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -12,14 +45,14 @@ export default function Register() {
                     </span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Username" className="loginInput" />
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" type="password" className="loginInput" />
-                        <input placeholder="Password Again" type="password" className="loginInput" />
-                        <button className="loginButton">Sign Up</button>
+                    <form className="loginBox" onSubmit={hancleClick}>
+                        <input placeholder="Username" required ref={username} className="loginInput" />
+                        <input placeholder="Email" required type="email" ref={email} className="loginInput" />
+                        <input placeholder="Password" onChange={checkIfPasswordMatch} required minLength="6" type="password" ref={password} type="password" className="loginInput" />
+                        <input placeholder="Password Again" onChange={checkIfPasswordMatch} required type="password" ref={passwordAgain} type="password" className="loginInput" />
+                        <button className="loginButton" type="submit">Sign Up</button>
                         <button className="loginRegisterButton">Log into Account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
